22228eb4c2be87a8cfd87b40c074ba6d5e5ff6e6
Vasyl Prokopiv <media.vpro@gmail.com>
2021-07-19 00:24:27 +0300

    wh in progres

--- homeworks/HW#5_OOP_2..py
+++ homeworks/HW#5_OOP_2..py
@@ -1,13 +1,174 @@
-class Human:
-    def __init__(self, name, surname, age):
-        self.name = name
-        self.surname = surname
-        self.age = age
-
-    def __str__(self):
-        return f"return {self.name} and {self.surname} and {self.age}"
+import dataclasses
+from collections import namedtuple
 
 
-vasyl = Human("Vasyl", "Prokopiv", 36)
+# 1.
+# class Laptop:
+#     Make the class with composition.
+# class Battery:
+#     Make the class with composition.
 
-print(vasyl)
+class Laptop:
+    def __init__(self):
+        battery = Battery
+
+
+class Battery:
+    def __init__(self):
+        pass
+
+
+# 2.
+# class Guitar:
+#     Make the class with aggregation
+# class GuitarString:
+#     Make the class with aggregation
+
+class Guitar:
+    def __init__(self, string):
+        self.string = string
+
+
+class GuitarString:
+    def __init__(self):
+        pass
+
+
+guitar_string_1 = GuitarString
+guitar_1 = Guitar(guitar_string_1)
+
+
+# 3.
+# class Calc:
+#     Make class with one method "add_nums" with 3 parameters, which returns sum of these parameters.
+#     Note: this method should be static
+
+class Calc:
+
+    @staticmethod
+    def add_nums(x, y, z):
+        return x + y + z
+
+
+print(Calc.add_nums(2, 3, 4))
+
+
+# 4*.
+# class Pasta:
+#     Make class which takes 1 parameter on init - list of ingredients and defines instance attribute ingredients.
+#     It should have 2 methods:
+#     carbonara (['forcemeat', 'tomatoes']) and bolognaise (['bacon', 'parmesan', 'eggs'])
+#     which should create Pasta instances with predefined list of ingredients.
+#     Example:
+#         pasta_1 = Pasta(["tomato", "cucumber"])
+#         pasta_1.ingredients will equal to ["tomato", "cucumber"]
+#         pasta_2 = Pasta.bolognaise()
+#         pasta_2.ingredients will equal to ['bacon', 'parmesan', 'eggs']
+
+class Pasta:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    @classmethod
+    def carbonara(cls):
+        return cls(['forcemeat', 'tomatoes'])
+
+    @classmethod
+    def bolognaise(cls):
+        return cls(['bacon', 'parmesan', 'eggs'])
+
+    def __repr__(self):
+        return f'with {self.ingredients}'
+
+
+pasta_1 = Pasta(['tomato', 'cucumber'])
+print('pasta_1', pasta_1)
+pasta_2 = Pasta.bolognaise()
+print('pasta_2', pasta_2)
+
+
+# 5*.
+# class Concert:
+#     Make class, which has max_visitors_num attribute and its instances will have visitors_count attribute.
+#     In case of setting visitors_count - max_visitors_num should be checked,
+#     if visitors_count value is bigger than max_visitors_num - visitors_count should be assigned with max_visitors_num.
+#     Example:
+#         Concert.max_visitor_num = 50
+#         concert = Concert()
+#         concert.visitors_count = 1000
+#         print(concert.visitors_count)  # 50
+
+class Concert:
+    def __init__(self, max_visitors_num):
+        self.max_visitors_num = max_visitors_num
+
+    @property
+    def visitors_count(self):
+        return self._visitors_count
+
+    @visitors_count.setter
+    def visitors_count(self, count):
+        self._visitors_count = count if count < self.max_visitors_num else f'# {self.max_visitors_num}'
+
+
+concert_1 = Concert(50)
+concert_1.visitors_count = 1000
+print(concert_1.visitors_count)
+
+
+# 6.
+# class AddressBookDataClass:
+#     Create dataclass with 7 fields - key (int), name (str), phone_number (str), address (str), email (str), birthday (str), age (int)
+
+@dataclasses.dataclass
+class AddressBookDataClass:
+    key: int
+    name: str
+    phone_number: str
+    address: str
+    email: str
+    birthday: str
+    age: int
+
+
+# 7. Create the same class (6) but using NamedTuple
+AddressBookDataClass = namedtuple('AddressBookDataClass',
+                                  ['key', 'name', 'phone_number', 'address', 'email', 'birthday', 'age'])
+
+# 8.
+# class AddressBook:
+#     Create regular class taking 7 params on init - key, name, phone_number, address, email, birthday, age
+#     Make its str() representation the same as for AddressBookDataClass defined above.
+
+
+
+
+# 9.
+# class Person:
+#     Change the value of the age property of the person object
+#     name = "John"
+#     age = 36
+#     country = "USA"
+#
+# 10.
+# class Student:
+#     Add an 'email' attribute of the object student and set its value
+#     Assign the new attribute to 'student_email' variable and print it by using getattr
+#     id = 0
+#     name = ""
+#
+#     def __init__(self, id, name):
+#         self.id = id
+#         self.name = name
+#
+# 11*.
+# class Celsius:
+#     By using @property convert the celsius to fahrenheit
+#     Hint: (temperature * 1.8) + 32)
+#     def __init__(self, temperature=0):
+#         self._temperature = temperature
+#
+# # create an object
+# {obj} = ...
+#
+# print({obj}.temperature)
